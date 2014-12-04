071026c68d35428d385905ebf9f10c83338c1519
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-12-03 19:18:26 -0600

    update background and header

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,32 @@
+$(document).on('click', '.button1', function(){
+  $('html, body').animate({
+    scrollTop: $(".two").offset().top
+  }, 2000);
+});
+
+$(document).on('click', '.button2', function(){
+  $('html, body').animate({
+    scrollTop: $(".three").offset().top
+  }, 2000);
+});
+
+$(document).on('click', '.skills', function(e){
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $(".two").offset().top
+  }, 2000);
+});
+
+$(document).on('click', '.projects', function(e){
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $(".three").offset().top
+  }, 2500);
+});
+
+$(document).on('click', '.contact', function(e){
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $(".four").offset().top
+  }, 3500);
+});
