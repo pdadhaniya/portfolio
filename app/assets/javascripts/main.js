48b0ebce357df8bab4e0003ad5e3535e9186b884
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-01-06 23:28:15 -0600

    update stylesheets to make site responsive

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,30 +10,30 @@ $(document).on('click', '.button2', function(){
   }, 2000);
 });
 
-$(document).on('click', '.about', function(e){
+$(document).on('click', '.about-me', function(e){
   e.preventDefault();
   $('html, body').animate({
-    scrollTop: $(".filler").offset().top
+    scrollTop: $("body").offset().top
   }, 2000);
 });
 
 $(document).on('click', '.skills', function(e){
   e.preventDefault();
   $('html, body').animate({
-    scrollTop: $(".two").offset().top
+    scrollTop: $(".skills-section").offset().top
   }, 2000);
 });
 
 $(document).on('click', '.projects', function(e){
   e.preventDefault();
   $('html, body').animate({
-    scrollTop: $(".three").offset().top
+    scrollTop: $(".projects-section").offset().top
   }, 2000);
 });
 
-$(document).on('click', '.contact', function(e){
+$(document).on('click', '.contact-me', function(e){
   e.preventDefault();
   $('html, body').animate({
-    scrollTop: $(".four").offset().top
+    scrollTop: $(".contact-section").offset().top
   }, 3500);
 });
